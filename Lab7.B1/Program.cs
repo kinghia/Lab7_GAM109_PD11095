712f6df258597a60a9092f28c9f7fcadb3b1f5ec
kinghia <162483327+kinghia@users.noreply.github.com>
2024-06-14 08:57:12 +0700

    commit lan dau

---
+++ Lab7.B1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7.B1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<int> nums = new List<int>() { 1, 3, 4, 5, 8, 9 };
+            string str = "chao mung den voi binh nguyen vo tan";
+            string strc = "CHAO mung DEN Voi binh nguyen vo tan";
+
+            Console.WriteLine("Bài 1: ");
+            Console.WriteLine("Câu a: xuất ra bình phương của các số lớn hơn 4 ");
+            var binhPhuongThan4 = from bp in nums
+                                  where bp > 4
+                                  select bp * bp ;
+            foreach (var item in binhPhuongThan4)
+            {
+                Console.WriteLine("Bình phương lớn hơn 4: " + item, item + item);
+            }
+
+            Console.WriteLine("\nCâu b: xuất ra màn hình lần lượt số lần xuất hiện của từng chữ");
+            var rs = str.Where(Char.IsLetter).GroupBy(x => x)
+                .Select(group => new { Chữ = group.Key, Đếm = group.Count() });
+            foreach ( var item in rs)
+            {
+                Console.WriteLine(item + "");
+            }
+
+            Console.WriteLine("\nCâu c: xuất ra màn hình những chuỗi được viết hoa toàn bộ");
+            var words = strc.Split(' ');
+            var result = from word in words
+                         where word.Equals(word.ToUpper())
+                         select word;
+            foreach ( var itemc in result)
+            {
+                Console.WriteLine(itemc  + " ");
+            }
+        }
+    }
+}
---
+++ Lab7.B2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7.B2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CustomerDBDataContext dataContext = new CustomerDBDataContext())
+            {
+                try
+                {
+                    var query = from order in dataContext.Orders
+                                join customer in dataContext.Customers
+                                on order.CustomerID equals customer.CustomerID
+                                select new
+                                {
+                                    NameOrder = order.Name,
+                                    NameCustomer = customer.Name,
+                                };
+                    foreach (var result in query)
+                    {
+                        Console.WriteLine("OrderID: " + result.OrderID + " | CustomerName: " + result.CustomerName);
+                    }
+                }
+            }
+        }
+    }
+}
